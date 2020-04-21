de7d69c2cf5793fec4cfeb7b4c81e58914173caf
JialeChenCS506 <55773134+JialeChenCS506@users.noreply.github.com>
2020-04-21 13:28:06 -0400

    Update paper_insert.sql
    simple error corrrect

--- paper_insert.sql
+++ paper_insert.sql
@@ -17,16 +17,16 @@ INSERT INTO reviewer (emailaddr, firstname, lastname, phonenum, affiliation) VAL
 
 
 INSERT INTO paper (paperid, title, abstract, filename, contactemailAddr) VALUES
-(1, 'ssdaaff', NULL, NULL, 'manutdgmail.com'),
-(3, 'phdasdaa', NULL, NULL, 'misaka.mija@gmail.com'),
-(4, 'lcksv', NULL, NULL, 'LBQ@gmail'),
+(1, 'ssdaaff', NULL, NULL, 'ab@gmail.com'),
+(3, 'phdasdaa', NULL, NULL, 'c.d@gmail.com'),
+(4, 'lcksv', NULL, NULL, 'e.f@gmail.com'),
 (7, 'tsanm', NULL, NULL, 'g.h@gmail.com');
 
 INSERT INTO Review (reviewId, paperId, techMeritScore, readabilityScore, originalityScore, relevanceScore, reviewerEmailAddr, recommendation, authorFeedback, commiteeFeedback)
- VALUES (1, 1, 8, 8, 8, 8, 'ab@gmail.com', NULL, NULL, NULL),
-(2, 1, 8, 8, 8, 8, 'c.d@gmail.comom', NULL, NULL, NULL),
-(3, 2, 8, 8, 8, 8, 'e.f@gmail.com.com', NULL, NULL, NULL),
-(4, 2, 8, 8, 8, 8, 'g.h@gmail.coml@gmail.com', NULL, NULL, NULL);
+ VALUES (1, 1, 8, 8, 8, 8, 'manutd@gmail.com', NULL, NULL, NULL),
+(2, 3, 8, 8, 8, 8, 'MANCIT@gmail.com', NULL, NULL, NULL),
+(3, 4, 8, 8, 8, 8, 'LBQ@gmail.com', NULL, NULL, NULL),
+(4, 7, 8, 8, 8, 8, 'CTE@gmail.com', NULL, NULL, NULL);
 
 
 
@@ -40,8 +40,8 @@ INSERT INTO Topic (topicid, topicname) VALUES
 
 
 INSERT INTO reviewerTopics (EmailAddr, TopicId) VALUES
-('ab@gmail.com', 1),
-('e.f@gmail@gmail.com', 1),
-('b.c@gmail.com', 2),
-('c.d@gmail.com', 2),
-('ab@gmail.com', 4);
+('manutd@gmail.com', 1),
+('CTE@gmail.com', 1),
+('MANCIT@gmail.com', 2),
+('LBQ@gmail.com', 2),
+('manutd@gmail.com', 4);
