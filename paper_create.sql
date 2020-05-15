67e02d093ded9d1d328c80b86895208cdb0e1dba
JialeChenCS506 <55773134+JialeChenCS506@users.noreply.github.com>
2020-05-15 00:02:09 -0400

    Update paper_create.sql

--- paper_create.sql
+++ paper_create.sql
@@ -1,15 +1,30 @@
 create database paperreviews;
 USE paperreviews;
 
-create table author(emailaddr VARCHAR(30), firstname VARCHAR(10),lastname VARCHAR(10),PRIMARY KEY(emailaddr));
+create table paper(paperid INTEGER(10), title VARCHAR(30), abstract VARCHAR(300), filename VARCHAR(30),PRIMARY KEY(paperid));
+
+
+
+
 
 
 create table paper(paperid INTEGER(10), title VARCHAR(30), abstract VARCHAR(300), filename VARCHAR(30),PRIMARY KEY(paperid));
 
 
- create table review(reviewid INTEGER(10),recommendation VARCHAR(30),meritscore INTEGER(10), readabilityscore INTEGER(10),reviewerid INTEGER(10),relevancescore INTEGER(10), paperid INTEGER(10), PRIMARY KEY(reviewid));
 
-create table reviewer(emailaddr VARCHAR(30) NOT NULL, firstname VARCHAR(30),lastname VARCHAR(30),phonnumm INTEGER(10),affiliation VARCHAR(30), authorfeedback VARCHAR(300), commiteefeedbacck VARCHAR(30), PRIMARY KEY(emailaddr));
+
+ create table review(reviewid INTEGER(10), recommendation VARCHAR(30),revieweremailAddr VARCHAR(30), meritscore INTEGER(10), readabilityscore INTEGER(10), originalityscore INTEGER(10),relevancescore INTEGER(10), paperid INTEGER(10), authorfeedback VARCHAR(30), commiteefeedback VARCHAR(30),PRIMARY KEY(reviewid));
+
+
+
+
+create table reviewer(emailaddr VARCHAR(30) NOT NULL, firstname VARCHAR(30),lastname VARCHAR(30),phonnum INTEGER(10),affiliation VARCHAR(30), authorfeedback VARCHAR(30), commiteefeedback VARCHAR(30), PRIMARY KEY(emailaddr));
+
+
+
+
+CREATE TABLE topic (topicid INTEGER(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,topicname VARCHAR(30));
+
 
 create table reviewertopics (
 emailaddr VARCHAR(30) NOT NULL,
@@ -19,8 +34,5 @@ FOREIGN KEY (emailaddr) REFERENCES reviewer(emailaddr),
 FOREIGN KEY (topicid) REFERENCES topic(topicid)
 );
 
-CREATE TABLE topic (topicid INTEGER(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,topicname VARCHAR(30));
 
-
-CREATE TABLE reviewertopics (emailaddr VARCHAR(30) NOT NULL, topicid INTEGER(8) NOT NULL,PRIMARY KEY (emailaddr,topicid),FOREIGN KEY (emailaddr) REFERENCES reviewer(emailaddr),FOREIGN KEY (topicid) REFERENCES topic(topicid));
 
