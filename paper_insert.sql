e4724819b54eae6208313972c9bf6eaa8a96f763
JialeChenCS506 <55773134+JialeChenCS506@users.noreply.github.com>
2020-05-15 00:03:30 -0400

    Update paper_insert.sql

--- paper_insert.sql
+++ paper_insert.sql
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 INSERT INTO author (emailaddr, firstname, lastname) VALUES
 ('ab@gmail.com', 'a', 'b'),
 ('c.d@gmail.com', 'c', 'd'),
@@ -7,26 +13,40 @@ INSERT INTO author (emailaddr, firstname, lastname) VALUES
 
 
 
-INSERT INTO reviewer (emailaddr, firstname, lastname, phonenum, affiliation) VALUES
-('manutd@gmail.com', 'man', 'utd', 178, NULL),
-('MANCIT@gmail.com', 'MAN', 'CIT', 888, NULL),
-('LBQ@gmail.com', 'LEBR', 'QYNN', 444, NULL),
-('CTE@gmail.com', 'Caster', 'TElika', 654, NULL);
 
 
 
 
-INSERT INTO paper (paperid, title, abstract, filename, contactemailAddr) VALUES
-(1, 'ssdaaff', NULL, NULL, 'ab@gmail.com'),
-(3, 'phdasdaa', NULL, NULL, 'c.d@gmail.com'),
-(4, 'lcksv', NULL, NULL, 'e.f@gmail.com'),
-(7, 'tsanm', NULL, NULL, 'g.h@gmail.com');
+INSERT INTO paper (paperid, title, abstract, filename) VALUES
+(1, 'ssdaaff', NULL, NULL),
+(3, 'phdasdaa', NULL, NULL),
+(4, 'lcksv', NULL, NULL),
+(7, 'tsanm', NULL, NULL);
 
-INSERT INTO Review (reviewId, paperId, techMeritScore, readabilityScore, originalityScore, relevanceScore, reviewerEmailAddr, recommendation, authorFeedback, commiteeFeedback)
- VALUES (1, 1, 8, 8, 8, 8, 'manutd@gmail.com', NULL, NULL, NULL),
-(2, 3, 8, 8, 8, 8, 'MANCIT@gmail.com', NULL, NULL, NULL),
-(3, 4, 8, 8, 8, 8, 'LBQ@gmail.com', NULL, NULL, NULL),
-(4, 7, 8, 8, 8, 8, 'CTE@gmail.com', NULL, NULL, NULL);
+
+
+
+ 
+
+
+
+INSERT INTO review (reviewid, paperid, meritscore, readabilityscore, originalityscore, relevancescore, revieweremailAddr, recommendation, authorfeedback, commiteefeedback)
+ VALUES (5, 1, 8, 8, 8, 8, 'manutd@gmail.com', NULL, NULL, NULL),
+(6, 3, 8, 8, 8, 8, 'MANCIT@gmail.com', NULL, NULL, NULL),
+(8, 4, 8, 8, 8, 8, 'LBQ@gmail.com', NULL, NULL, NULL),
+(9, 7, 8, 8, 8, 8, 'CTE@gmail.com', NULL, NULL, NULL);
+
+
+
+
+INSERT INTO reviewer (emailaddr, firstname, lastname, phonnum, affiliation,authorfeedback , commiteefeedback) VALUES
+('manutd@gmail.com', 'man', 'utd',  178, NULL,NULL, NULL),
+('MANCIT@gmail.com', 'MAN', 'CIT', 888, NULL,NULL,NULL),
+('LBQ@gmail.com', 'LEBR', 'Q', 444, NULL,NULL,NULL),
+('CTE@gmail.com', 'Caster', 'Elika', 654, NULL,NULL,NULL);
+
+
+
 
 
 
@@ -39,9 +59,13 @@ INSERT INTO Topic (topicid, topicname) VALUES
 (5, 'music');
 
 
+
+
+
 INSERT INTO reviewerTopics (EmailAddr, TopicId) VALUES
 ('manutd@gmail.com', 1),
 ('CTE@gmail.com', 1),
 ('MANCIT@gmail.com', 2),
 ('LBQ@gmail.com', 2),
 ('manutd@gmail.com', 4);
+
